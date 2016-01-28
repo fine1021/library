7bdab43095724cf2fd5579eac08ed81dbc7971f8
fine1021 <1078041387@qq.com>
2016-01-28 22:20:28 +0800

    update comment

--- support/src/main/java/com/yxkang/android/util/ThreadManager.java
+++ support/src/main/java/com/yxkang/android/util/ThreadManager.java
@@ -49,18 +49,18 @@ public class ThreadManager {
      * Future representing the pending results of the task. The
      * Future's {@code get} method will return the task's result upon
      * successful completion.
-     * <p/>
-     * <p/>
+     * <br>
      * If you would like to immediately block waiting
      * for a task, you can use constructions of the form
      * {@code result = exec.submit(aCallable).get();}
-     * <p/>
+     * <br>
      * <p>Note: The {@link Executors} class includes a set of methods
      * that can convert some other common closure-like objects,
      * for example, {@link java.security.PrivilegedAction} to
      * {@link Callable} form so they can be submitted.
      *
      * @param task the task to submit
+     * @param <T>  data type
      * @return a Future representing pending completion of the task
      * @throws RejectedExecutionException if the task cannot be
      *                                    scheduled for execution
