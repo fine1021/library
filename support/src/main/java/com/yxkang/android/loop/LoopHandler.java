6413846f2d914d549b329ecdf4e72835ad94d175
fine1021 <1078041387@qq.com>
2016-09-16 10:49:47 +0800

    add LoopHandler comment, and support multi-loop

--- support/src/main/java/com/yxkang/android/loop/LoopHandler.java
+++ support/src/main/java/com/yxkang/android/loop/LoopHandler.java
@@ -1,50 +1,157 @@
 package com.yxkang.android.loop;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
+import android.util.SparseArray;
+import android.util.SparseBooleanArray;
+
+import java.lang.ref.WeakReference;
 
 /**
  * Created by fine on 2016/9/16.
+ * <p>LoopHandler used for Multi-loop and Multi-action,
+ * it also provides many convenient methods for Single-loop and Single-action</p>
  */
 public final class LoopHandler {
 
     private static final String TAG = "LoopHandler";
 
-    private LoopAction action;
-
-    private boolean loop = false;
+    private final SparseBooleanArray array = new SparseBooleanArray();
+    private final SparseArray<WeakReference<LoopAction>> actions = new SparseArray<>();
 
     private LoopHandler() {
+        array.clear();
+        actions.clear();
     }
 
+    /**
+     * check the first action status is ready for next loop action
+     *
+     * @return {@code true} if ready, otherwise {@code false}
+     */
     public boolean isPresent() {
+        return isPresent(0);
+    }
+
+    /**
+     * check the key action status is ready for next loop action
+     *
+     * @param key the key
+     * @return {@code true} if ready, otherwise {@code false}
+     */
+    public boolean isPresent(int key) {
+        LoopAction action = getAction(key);
         return action == null || action.getStatus() == LoopAction.Status.FINISHED;
     }
 
+    /**
+     * check the first loop flag
+     *
+     * @return loop flag value, {@code true} or {@code false}
+     */
     public boolean isLoop() {
-        return loop;
+        return isLoop(0);
     }
 
+    /**
+     * check the loop of the key flag
+     *
+     * @param key the key
+     * @return loop flag value, {@code true} or {@code false}
+     */
+    public boolean isLoop(int key) {
+        return array.get(key);
+    }
+
+    /**
+     * set the loop flag of the first loop
+     *
+     * @param loop the loop flag
+     */
     public void setLoop(boolean loop) {
-        this.loop = loop;
+        setLoop(0, loop);
     }
 
+    /**
+     * set the loop flag of the loop of the key
+     *
+     * @param key  the key
+     * @param loop the loop flag
+     */
+    public void setLoop(int key, boolean loop) {
+        array.put(key, loop);
+    }
+
+    /**
+     * get the loop action of the first loop
+     *
+     * @return the loop action, or {@code null} if not exists
+     */
     public LoopAction getAction() {
-        return action;
+        return getAction(0);
     }
 
+    /**
+     * get the loop action of the key
+     *
+     * @param key the key
+     * @return the loop action, or {@code null} if not exists
+     */
+    @Nullable
+    public LoopAction getAction(int key) {
+        WeakReference<LoopAction> reference = actions.get(key);
+        if (reference == null) {
+            return null;
+        }
+        return reference.get();
+    }
+
+    /**
+     * set the first loop a new loop action
+     *
+     * @param action the loop action
+     */
     public void setAction(LoopAction action) {
-        this.action = action;
+        setAction(0, action);
     }
 
+    /**
+     * set the loop of the key a new loop action
+     *
+     * @param key    the key
+     * @param action the loop action
+     */
+    public void setAction(int key, LoopAction action) {
+        WeakReference<LoopAction> reference = new WeakReference<>(action);
+        actions.put(key, reference);
+    }
+
+    /**
+     * notify the first loop has been finished
+     */
     public void loopFinished() {
+        loopFinished(0);
+    }
+
+    /**
+     * notify the loop of the key has been finished
+     *
+     * @param key the key
+     */
+    public void loopFinished(int key) {
+        LoopAction action = getAction(key);
         if (action != null && action.isReady()) {
             action.preExecute();
             action.execute();
             action.postExecute();
+            actions.delete(key);
         }
     }
 
+    /**
+     * @see #wait()
+     */
     public void sleep() {
         synchronized (this) {
             try {
@@ -55,6 +162,9 @@ public final class LoopHandler {
         }
     }
 
+    /**
+     * @see #wait(long)
+     */
     public void sleep(long millis) {
         synchronized (this) {
             try {
@@ -65,6 +175,9 @@ public final class LoopHandler {
         }
     }
 
+    /**
+     * @see #notifyAll()
+     */
     public void wake() {
         synchronized (this) {
             try {
