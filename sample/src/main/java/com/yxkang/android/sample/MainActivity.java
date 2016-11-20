4a0c391292197b71f100d73ae498e811b89ee34d
fine1021 <1078041387@qq.com>
2016-11-20 12:24:30 +0800

    add day night mode switch

--- sample/src/main/java/com/yxkang/android/sample/MainActivity.java
+++ sample/src/main/java/com/yxkang/android/sample/MainActivity.java
@@ -14,6 +14,7 @@ import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -383,7 +384,7 @@ public class MainActivity extends AppCompatActivity implements EditDialogFragmen
 
         final String command;
 
-        public RootTask(String command) {
+        RootTask(String command) {
             this.command = command;
         }
 
@@ -398,7 +399,7 @@ public class MainActivity extends AppCompatActivity implements EditDialogFragmen
     private class WaitingTask implements Runnable {
         final Future<Boolean> future;
 
-        public WaitingTask(Future<Boolean> future) {
+        WaitingTask(Future<Boolean> future) {
             this.future = future;
         }
 
@@ -420,6 +421,7 @@ public class MainActivity extends AppCompatActivity implements EditDialogFragmen
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        menu.removeItem(R.id.action_settings);
         return true;
     }
 
@@ -432,6 +434,14 @@ public class MainActivity extends AppCompatActivity implements EditDialogFragmen
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        } else if (id == R.id.action_day) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            recreate();
+            return true;
+        } else if (id == R.id.action_night) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            recreate();
             return true;
         }
 
